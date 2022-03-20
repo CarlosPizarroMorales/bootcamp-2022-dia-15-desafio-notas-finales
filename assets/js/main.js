9f805435a027bbb7a0ab8f11f464afdca250cdd9
Carlos Pizarro <carlospizarromorales@gmail.com>
2022-03-20 06:31:56 -0300

    completo pero feo

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,14 @@
 const parciales = 3;
-const nombreCarrera = 'Desarrollo web';
+const perfil = ['Desarrollo web'];
 const data = [['HTML'], ['CSS'], ['Javascript']];
-const nombreAlumno = prompt('Ingrese el nombre del alumno:');
+perfil.push(prompt('Ingrese el nombre del alumno:'));
 
 for (let i = 0; i < (parciales * data.length); i++) {
     let parcial = prompt(`Ingrese la nota ${(i % parciales) + 1} del ramo ${data[Math.trunc(i / parciales)][0]}.`, 1);
     data[Math.trunc(i / 3)].push(Number(parcial));
 }
+
+document.querySelectorAll('p').forEach((e, i) => e.innerText += perfil[i]);
 
 const tabla = document.querySelectorAll('td');
 for (let i = 0; i < parciales; i++) {
@@ -16,6 +18,7 @@ for (let i = 0; i < parciales; i++) {
     tabla[(i * 4) + 2].innerText = data[i][3];
     tabla[(i * 4) + 3].innerText = data[i][4].toFixed(2);
 }
+
 
 // COMO HACER UNA SERIE SIMPLE QUE OBTENGA EL VALOR 1,2,3,4 ITERATIVAMENTE???
 // for (let i = 0; i < 12; i++) {
